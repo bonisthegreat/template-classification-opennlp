cd00827f64fbb108cdb7a11cc18a2594aaeeff81
Pablo Duque Asens <pduque@sga.es>
2017-06-22 13:17:58 +0200

    Return top 3 predictions with their respective confidences

---
+++ src/main/scala/io/prediction/opennlp/engine/AlgorithimPatch.scala
@@ -0,0 +1,2 @@
+
+val tupleList = Nil
--- src/main/scala/io/prediction/opennlp/engine/Algorithm.scala
+++ src/main/scala/io/prediction/opennlp/engine/Algorithm.scala
@@ -1,11 +1,8 @@
-package io.prediction.opennlp.engine
-
 import org.apache.predictionio.controller.P2LAlgorithm
 import opennlp.maxent.GIS
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.predictionio.data.storage.DataMap
-
 
 class Algorithm(val ap: AlgorithmParams)
   extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult] {
@@ -16,8 +13,34 @@ class Algorithm(val ap: AlgorithmParams)
   }
 
   def predict(model: Model, query: Query): PredictedResult = {
-    val interest = model.gis.getBestOutcome(model.gis.eval(query.sentence.split(" "))).replace("_"," ") 
-    PredictedResult(interest)
+    val interest = model.gis.getAllOutcomes(model.gis.eval(query.sentence.split(" ")))
+    println(interest)
+    //interest.split("  |\t")
+    val array = interest.split("\\]")
+    //array.foreach{println}
+    val listOfTuples = scala.collection.mutable.ListBuffer[(String, Double)]()
+
+    array.foreach{
+        x => val splitted = x.split("\\[")
+        val thisTuple = (splitted(0).trim, splitted(1).toDouble)
+        listOfTuples += thisTuple
+    }
+    //listOfTuples.foreach(println)
+    val count = 0
+    val topThreePredictions = new StringBuilder()
+    for(count <- 1 to 3){
+
+        val thisTuple = listOfTuples.maxBy(_._2)
+        topThreePredictions.append(thisTuple._1)
+        topThreePredictions.append("|")
+        topThreePredictions.append(thisTuple._2.toString)
+        topThreePredictions.append("|")
+        listOfTuples -= thisTuple
+    }
+
+    //println(topThreePredictions.stripSuffix("|"))
+    val finalResult = topThreePredictions.stripSuffix("|")
+    PredictedResult(finalResult)
   }
 
   override def batchPredict(model: Model, qs: RDD[(Long, Query)]): RDD[(Long, PredictedResult)] = {
